3e7f4887ff85c6032f3963c77020535f7d109499
Pedro Agriarte <agriarte@hotmail.com>
2018-03-20 17:36:08 +0100

    revision

--- src/com/pedro/Main6.java
+++ src/com/pedro/Main6.java
@@ -2,26 +2,39 @@ package com.pedro;
 
 import sun.applet.Main;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 
 public class Main6 {
 
     public static void main(String[] args) {
+
+        ArrayList<String> nombreSplit = new ArrayList<>();
         //Array de String
         String[] nombres = {"Sr. Pepe", "Don Juan", "Dr. Alex",
                 "Sra. Francisca"};
-
+        System.out.println("Array original");
+        for (String i : nombres) {
+            System.out.print(i + ", ");
+        }
+        System.out.println("\n");
+        System.out.println("Array.sort(nombres)");
         //Ordena el array
         Arrays.sort(nombres);
         //Mostramos el array ya ordenado
         for (String i : nombres) {
             System.out.print(i + ", ");
         }
+        System.out.println("\n");
 
         //parte cada elemento del array en 2 partes con split. parte[0] y parte[1]
         //split tiene como parámetro el separador, en este caso " "
+        //behavior como parámetro
+        System.out.println("new Comparator, split por el espacio, ordenamos por nombre");
         Arrays.sort(nombres, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
@@ -31,30 +44,44 @@ public class Main6 {
         System.out.println(Arrays.toString(nombres));
 
         //lambda
+        System.out.println("\nIgual pero con lambda");
         Arrays.sort(nombres, Comparator.comparing(n -> n.split(" ")[1]));
-        System.out.println("lambda");
+        System.out.println("lambda Comparator.comparing n.split(\" \")[1]");
         System.out.println(Arrays.toString(nombres));
 
 
-        //
+        //Referencia a método estático ::MétodoEstático
+        System.out.println("\nArrays.sort(nombres,Comparator.comparing(Main6::firstName));");
         Arrays.sort(nombres,Comparator.comparing(Main6::firstName));
         System.out.println(Arrays.toString(nombres));
 
         //
+        System.out.println("\nArrays.sort(nombres,Comparator.comparing(Main6::firstName).reversed());");
         Arrays.sort(nombres,Comparator.comparing(Main6::firstName).reversed());
         System.out.println(Arrays.toString(nombres));
 
         //
+        System.out.println("\nArrays.sort(nombres,Comparator.comparing(Main6::firstName).reversed().thenComparing(Main6::tratamiento));");
         Arrays.sort(nombres,Comparator.comparing(Main6::firstName).reversed().thenComparing(Main6::tratamiento));
         System.out.println(Arrays.toString(nombres));
 
 
+        //manera1
+        //crear nuevo ArrayList con solo con nombres
+        //imprimir solo nombres
+        for(String pivote : nombres)
+        {
+            System.out.println(pivote.split(" ")[1]);
+            nombreSplit.add(pivote.split(" ")[1]);
+        }
+        System.out.println(nombreSplit);
+
     }
 
-    public static String firstName(String nombre) {
+    private static String firstName(String nombre) {
         return nombre.split(" ")[1];
     }
-    public static String tratamiento(String nombre) {
+    private static String tratamiento(String nombre) {
         return nombre.split(" ")[0];
     }
 }
